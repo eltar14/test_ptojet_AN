cdc1e22f4ae56f75e2801c36dafaa3fe7fe6c09a
antoine <lebouant2@gmail.com>
2024-06-04 01:35:25 +0200

    tests fcts

--- main.py
+++ main.py
@@ -28,10 +28,45 @@ def brute_force(objects_list:list, backpack_size:float):
                 bag.append(objects_list[j])
                 #print(objects_list[j])
         bags.append(bag)
+        if(i>1000):
+            break
     return bags
 
 
+# Fonction pour trouver le maximum de la somme des éléments à l'index [2]
+def max_sum_element_n(liste_de_listes, n):
+    sommes = []
 
+    for sous_liste in liste_de_listes:
+        if sous_liste:  # Vérifier que la sous-liste n'est pas vide
+            elements_n = [item[n] for item in sous_liste if len(item) > n]  # Extraire les éléments à l'index n
+            somme = sum(elements_n)
+            sommes.append(somme)
+
+    if sommes:  # Vérifier que la liste des sommes n'est pas vide
+        max_somme = max(sommes)
+        print(sommes)
+    else:
+        max_somme = None  # Retourner None s'il n'y a pas de sommes valides
+
+    return max_somme
+
+
+def max_sum_element_n_bis(liste_de_listes, n):
+    max_somme = None
+    max_index = None
+    max_sous_liste = None
+
+    for index, sous_liste in enumerate(liste_de_listes):
+        if sous_liste:  # Vérifier que la sous-liste n'est pas vide
+            elements_n = [item[n] for item in sous_liste if len(item) > n]  # Extraire les éléments à l'index n
+            somme = sum(elements_n)
+            if max_somme is None or somme > max_somme:
+                max_somme = somme
+                max_index = index
+                max_sous_liste = sous_liste
+
+    return max_somme, max_index, max_sous_liste
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
@@ -45,7 +80,9 @@ if __name__ == '__main__':
     # print(f'Temps d\'exécution en secondes pour 10000 * la fonc: {(end_time-start_time)/1}; en ms : {(end_time-start_time)*1000/1}')# pour avoir une moyenne
 
     start_time = time.time()
-    brute_force(data, 0.6)
+    a = brute_force(data, 0.6)
+    print(a)
+    print(max_sum_element_n_bis(a, 1))
     end_time = time.time()
     print(f'Temps d\'exécution en secondes pour  * la fonc: {(end_time-start_time)/1}; en ms : {(end_time-start_time)*1000/1}')
 

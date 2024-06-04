e6c69711c50b83797cdff6e9937ad940bf367b25
eltar14 <lebouant2@gmail.com>
2024-06-04 09:59:07 +0200

    brute_force_avec_Obj

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 import pandas as pd
 import time
 
+class Objet:
+    def __init__(self, nom, masse, utilite):
+        self.nom = nom
+        self.masse = masse
+        self.utilite = utilite
+
+    def print(self):
+        print(f'{self.nom} ; {self.masse} ; {self.utilite}')
+
+
+class Bag:
+    def __init__(self):
+        self.content = [] # contient des Objets
+        self.weight = 0
+        self.score = 0
+
+    def add(self, obj:Objet):
+        self.content.append(obj)
+        self.weight += obj.masse
+        self.score += obj.utilite
+
+    def print(self):
+        print(f'Contenu du sac de masse: {self.weight} et d\'utilite {self.score}')
+        for elt in self.content:
+            #self.content[i].print()#f'{obj.nom} ; {obj.masse} ; {obj.utilite}'
+            elt.print()#f'{obj.nom} ; {obj.masse} ; {obj.utilite}'
+
+
+
+    def get_mass(self):
+        sum = 0
+        for o in self.content:
+            sum += o.masse
+        return sum
+
 def algo_concon(objects_list:list, backpack_size:float): # 19 op
     sac = []
     #print(objects_list)
@@ -13,7 +48,7 @@ def algo_concon(objects_list:list, backpack_size:float): # 19 op
 
     #print(f'Elements dans le sac : {sac}\nPoids : {p}')
 
-def brute_force(objects_list:list, backpack_size:float):
+def brute_force(objects_list:dict, backpack_size:float):
     #print(objects_list)
     bags = []
     for i in range(2**len(objects_list)-1):
@@ -33,45 +68,74 @@ def brute_force(objects_list:list, backpack_size:float):
     return bags
 
 
-# Fonction pour trouver le maximum de la somme des éléments à l'index [2]
-def max_sum_element_n(liste_de_listes, n):
-    sommes = []
-
-    for sous_liste in liste_de_listes:
-        if sous_liste:  # Vérifier que la sous-liste n'est pas vide
-            elements_n = [item[n] for item in sous_liste if len(item) > n]  # Extraire les éléments à l'index n
-            somme = sum(elements_n)
-            sommes.append(somme)
-
-    if sommes:  # Vérifier que la liste des sommes n'est pas vide
-        max_somme = max(sommes)
-        print(sommes)
-    else:
-        max_somme = None  # Retourner None s'il n'y a pas de sommes valides
-
-    return max_somme
 
 
-def max_sum_element_n_bis(liste_de_listes, n):
-    max_somme = None
-    max_index = None
-    max_sous_liste = None
 
-    for index, sous_liste in enumerate(liste_de_listes):
-        if sous_liste:  # Vérifier que la sous-liste n'est pas vide
-            elements_n = [item[n] for item in sous_liste if len(item) > n]  # Extraire les éléments à l'index n
-            somme = sum(elements_n)
-            if max_somme is None or somme > max_somme:
-                max_somme = somme
-                max_index = index
-                max_sous_liste = sous_liste
 
-    return max_somme, max_index, max_sous_liste
+def brute_force_avec_Obj(objects_list:list, backpack_size:float):
+    bags = []
+    for i in range(2**len(objects_list)-1):
+        bag = Bag()
+        binaire_sans_prefixe = bin(i)[2:].zfill(len(objects_list))
+        for j in range(len(objects_list)):
+            if(int(binaire_sans_prefixe[j]) == 1):
+                bag.add(objects_list[j])
+        if(bag.weight <= (backpack_size+0.005)): # +epsilon pour l'erreur de float de python
+            bags.append(bag)
+    return bags
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     data = pd.read_csv("sac2.csv", sep=';', decimal=',')
     data = data.values.tolist()
+    # dict = {}
+    # for elt in data:
+    #     if elt != []:
+    #         dict[elt[0] ]= (float(elt[1]), float(elt[2]))
+    #
+    # print(dict)
+    objs = []
+    #print(data)
+    for elt in data:
+        #print(*elt)
+        o = Objet(*elt)
+        objs.append(o)
+        #o.print()
+        #o.nom, o.masse, o.utilite = elt
+
+    #print (objs[0].nom)
+
+    """    ob = Objet("test", 2, 25)
+    ob.print()
+    bag = Bag()
+    bag.add(ob)
+    bag.print()
+    ob2 = Objet("test2", 3, 4)
+    bag.add(ob2)
+    bag.print()"""
+
+
 #    start_time = time.time()
     # for i in range(10000):
     #     algo_concon(data, 0.6)
@@ -80,10 +144,18 @@ if __name__ == '__main__':
     # print(f'Temps d\'exécution en secondes pour 10000 * la fonc: {(end_time-start_time)/1}; en ms : {(end_time-start_time)*1000/1}')# pour avoir une moyenne
 
     start_time = time.time()
-    a = brute_force(data, 0.6)
-    print(a)
-    print(max_sum_element_n_bis(a, 1))
+    #a = brute_force_avec_Obj(data, 0.6)
+    b = brute_force_avec_Obj(objs, 0.6)
+    #print(a)
+    ##print(max_sum_element_n_bis(a, 1))
     end_time = time.time()
+    print(len(b))
+    best = max(b, key=lambda k: k.score)
+    best.print()
+    """for sac in b:
+        sac.print()
+        print('\n\n\n')"""
+
     print(f'Temps d\'exécution en secondes pour  * la fonc: {(end_time-start_time)/1}; en ms : {(end_time-start_time)*1000/1}')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
